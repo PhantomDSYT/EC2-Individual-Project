09bf259bf646d98dc1d5672ddd819f071ebb4477
Anelka Lopez <anl.ka2543@hotmail.com>
2022-01-19 18:59:30 -0500

    Initial Commit

---
+++ EC2_1908764/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EC2_1908764.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
