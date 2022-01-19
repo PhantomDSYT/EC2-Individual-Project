f47251610d5d5785b73195b1d11e02a45400a592
Anelka Lopez <anl.ka2543@hotmail.com>
2022-01-19 18:59:30 -0500

    Initial Commit

---
+++ EC2_1908764/Data/EC2_1908764Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EC2_1908764.Models;
+
+namespace EC2_1908764.Data
+{
+    public class EC2_1908764Context : DbContext
+    {
+        public EC2_1908764Context (DbContextOptions<EC2_1908764Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EC2_1908764.Models.Movie> Movie { get; set; }
+    }
+}
